48af8d34b5615a2a30289bbde30db01d983d2f31
Tiffany Timbric <tiffanytimbric@gmail.com>
2024-11-26 08:49:02 -0700

    develop Added to and updated `LangUtil`.

--- src/main/java/com/tiffanytimbric/util/LangUtil.java
+++ src/main/java/com/tiffanytimbric/util/LangUtil.java
@@ -8,9 +8,27 @@ public class LangUtil {
 
     @Nonnull
     public static <T> Optional<T> opt(
-            @Nullable final T value
+            @Nullable final T obj
     ) {
-        return Optional.ofNullable(value);
+        if (obj == null) {
+            return optNull(obj);
+        }
+
+        return Optional.of(obj);
+    }
+
+    @Nonnull
+    public static <T> Optional<T> optNull(
+            @Nullable final T obj
+    ) {
+        return Optional.ofNullable(obj);
+    }
+
+    @Nonnull
+    public static <T> Optional<T> optEmpty(
+            @Nonnull final Class<T> clazz
+    ) {
+        return Optional.empty();
     }
 
 }
